dc7e0c0035189df88c0e9b6e415a7bfac1e2cb81
lig642 <112500396+lig642@users.noreply.github.com>
2023-05-09 19:15:40 -0400

    s

---
+++ week5.js
@@ -0,0 +1,14 @@
+console.log('Remove this console log and get started!')
+
+const data = {
+  channels: [
+    // Add your channel data here
+    // {
+    
+    //   title: 'UFOs',
+    //   videoIds: [
+    //     // Add your video IDs here
+    //   ]
+    // }
+  ]
+}
